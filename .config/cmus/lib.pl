ac384010e537d3d2bd24b371b67be43abed5e19d
krynnlord <krynnlord@yahoo.com>
2018-11-25 16:03:45 -0600

    Initial Push

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,14 @@
+http://ice1.somafm.com/bagel-128-mp3
+http://ice1.somafm.com/beatblender-128-mp3
+http://ice1.somafm.com/deepspaceone-128-mp3
+http://ice1.somafm.com/dronezone-128-mp3
+http://ice1.somafm.com/dubstep-128-mp3
+http://ice1.somafm.com/lush-128-mp3
+http://ice1.somafm.com/metal-128-mp3
+http://ice1.somafm.com/secretagent-128-mp3
+http://ice1.somafm.com/sonicuniverse-128-mp3
+http://ice1.somafm.com/spacestation-128-mp3
+http://ice1.somafm.com/suburbsofgoa-128-mp3
+http://ice1.somafm.com/thetrip-128-mp3
+http://ice1.somafm.com/thistle-128-mp3
+http://ice1.somafm.com/u80s-128-mp3
